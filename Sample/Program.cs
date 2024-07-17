9568f8a91e1336f1c0a9f9af13df32bc311b0e20
Kazuki <kazubowling300@gmail.com>
2024-07-17 16:51:38 +0900

    Sample02 プログラムで計算をしてみよう 加減乗除と剰余を求める計算

--- Sample/Program.cs
+++ Sample/Program.cs
@@ -12,3 +12,25 @@ name = "きたむら";
 
 Console.WriteLine(money);
 Console.WriteLine(name);
+
+int answer;
+
+// 加算
+answer = 3 + 4;
+Console.WriteLine(answer);
+
+// 減算
+answer = 12 - 18;
+Console.WriteLine(answer);
+
+// 乗算
+answer = 2 * 7;
+Console.WriteLine(answer);
+
+// 除算
+answer = 18 / 3;
+Console.WriteLine(answer);
+
+//剰余
+answer = 21 % 5;
+Console.WriteLine(answer);
