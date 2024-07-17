cda68b9f344e26e9f7c25416f244f515886defe6
Kazuki <kazubowling300@gmail.com>
2024-07-17 14:39:10 +0900

    Sample01 ユーザーの名前と所持金を変数に入れる
    変数の中身を表示する

--- Sample/Program.cs
+++ Sample/Program.cs
@@ -1,3 +1,14 @@
-﻿// コンソールに文字列を表示する
+﻿/* コンソールに文字列を表示する
 Console.WriteLine("Hello, C#");
-Console.WriteLine("Goodbye, C#");
+Console.WriteLine("Goodbye, C#"); */
+
+// 変数の宣言
+int money; // 所持金を代入する変数
+string name; // 名前を代入する変数
+
+// 変数に値を代入する
+money = 5000;
+name = "きたむら";
+
+Console.WriteLine(money);
+Console.WriteLine(name);
