2184fbe1cc9c2994ae01a9591cc69390f5cc8c71
Kazuki <kazubowling300@gmail.com>
2024-07-17 13:32:25 +0900

    コミット

--- Sample/obj/Debug/net8.0/Sample.AssemblyInfo.cs
+++ Sample/obj/Debug/net8.0/Sample.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sample")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+767b6361a0a90ce6ba39c0a023d07cd2754bc567")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2a10acbb51bc022b545433616fa516607f039ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sample")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sample")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
