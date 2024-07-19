dbd3f3151e4689bb0b8db3efc5700ec4139466da
kazubowling <kazubowling300@gmail.com>
2024-07-19 11:33:49 +0900

    Sample 08 複数の条件式を書く例

--- Sample/Program.cs
+++ Sample/Program.cs
@@ -122,10 +122,15 @@ int player1PosX = 2; // プレイヤ1の位置
 int player2PosX = 6; // プレイヤ2の位置
 
 // プレイヤ1の位置が「2」かつプレイヤ2の位置が「6」の場合に仕掛けを解除
-if (player1PosX == 2)
+/* if (player1PosX == 2)
 {
     if (player2PosX == 6)
     {
         Console.WriteLine("仕掛け解除");
     }
+} */
+
+if (player1PosX == 2 && player2PosX == 6)
+{
+    Console.WriteLine("仕掛け解除");
 }
