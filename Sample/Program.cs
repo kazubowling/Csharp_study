b0f4e0aac71bd5bacbc58b7c4705a4b3aedc79ed
kazubowling <kazubowling300@gmail.com>
2024-07-19 10:52:28 +0900

    Sample 08 協力プレイで仕掛けを解除しよう 入れ子にしたif文の使用例

--- Sample/Program.cs
+++ Sample/Program.cs
@@ -111,9 +111,21 @@ if (playerPosX == enemyPosX)
 } */
 
 // 練習問題 3-7
-int num = 8;
+/* int num = 8;
 
 if (num >= 3)
 {
     Console.WriteLine("勝ち");
+} */
+
+int player1PosX = 2; // プレイヤ1の位置
+int player2PosX = 6; // プレイヤ2の位置
+
+// プレイヤ1の位置が「2」かつプレイヤ2の位置が「6」の場合に仕掛けを解除
+if (player1PosX == 2)
+{
+    if (player2PosX == 6)
+    {
+        Console.WriteLine("仕掛け解除");
+    }
 }
