4d4416df507b3d07d6129065ae9feb120c83fbd2
kazubowling <kazubowling300@gmail.com>
2024-07-25 15:06:56 +0900

    Sample15 体重を管理するプログラムを作ろう 配列の使用例

--- Sample/Program.cs
+++ Sample/Program.cs
@@ -287,7 +287,7 @@ while (true)
 }
 Console.WriteLine("HIT"); */
 
-// 練習問題 3-17
+/* 練習問題 3-17
 int sum = 0;
 int count = 1;
 while (true)
@@ -300,4 +300,22 @@ while (true)
         break;
     }    
     count++;
+} */
+
+float[] weights; // 配列用の変数を宣言する
+weights = new float[7]; // 配列の要素数を決める
+
+// 配列の要素に値を代入する
+weights[0] = 41.2f;
+weights[1] = 42.5f;
+weights[2] = 44.9f;
+weights[3] = 43.2f;
+weights[4] = 45.1f;
+weights[5] = 43.2f;
+weights[6] = 42.7f;
+
+// 配列の値をすべて表示する
+for (int i = 0; i < 7; i++)
+{
+    Console.WriteLine(weights[i]);
 }
