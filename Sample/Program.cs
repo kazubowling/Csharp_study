a3e47348730542a013826deb7fa08da8907e446e
Kazuki Murohara <kazubowling300@gmail.com>
2024-07-31 16:23:18 +0900

    練習問題 3-21

--- Sample/Program.cs
+++ Sample/Program.cs
@@ -392,7 +392,7 @@ void Shop(string itemName)
 
 Shop("薬草"); */
 
-// 練習問題 3-20
+/* 練習問題 3-20
 void ShowEvenOrOdd (int num)
 {
     if ((num % 2) == 0)
@@ -405,4 +405,43 @@ void ShowEvenOrOdd (int num)
     }
 }
 
-ShowEvenOrOdd(2);
+ShowEvenOrOdd(2); */
+
+// 商品名を引数に取り、
+// 対応する商品の値段を返すメソッド
+int Shop (string itemName)
+{
+    Console.WriteLine("いらっしゃいませ！");
+    int price = 0;
+    if (itemName == "薬草")
+    {
+        Console.WriteLine(itemName + "は100円です");
+        price = 100;
+    }
+    else if (itemName == "棍棒")
+    {
+        Console.WriteLine(itemName + "は1500円です");
+        price = 1500;
+    }
+    else if ( itemName == "万能薬")
+    {
+        Console.WriteLine(itemName + "は500円です");
+        price = 500;
+    }
+    else
+    {
+        Console.WriteLine("売り切れです");
+        price = 0;
+    }
+
+    return price;
+}
+
+int money = 2500;
+Console.WriteLine("所持金は" + money + "円です");
+int price = Shop("薬草");
+money -= price;
+Console.WriteLine("所持金は" + money + "円です");
+price = Shop("万能薬");
+money -= price;
+Console.WriteLine("所持機は" + money + "円です");
