e98318335c678b7823f07fa85881dca7095a1cd8
Kazuki Murohara <kazubowling300@gmail.com>
2024-07-31 12:17:12 +0900

    Sample 19 商品の値段を教えてくれるショップを作ろう
    アイテムに応じた値段を表示するメソッド

--- Sample/Program.cs
+++ Sample/Program.cs
@@ -370,3 +370,24 @@ void Thankyou()
     Console.WriteLine("ありがとうございました！");
 } */
 
+// 引数に商品名を受け取り、
+// 商品名に応じた値段を表示するメソッド
+void Shop(string itemName)
+{
+    Console.WriteLine("いらっしゃいませ！");
+
+    if (itemName == "薬草")
+    {
+        Console.WriteLine(itemName + "は100円です");
+    }
+    else if (itemName == "棍棒")
+    {
+        Console.WriteLine(itemName + "は1500円です");
+    }
+    else
+    {
+        Console.WriteLine("売り切れです");
+    }
+}
+
+Shop("薬草");
